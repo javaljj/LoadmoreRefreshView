dcaa7a3f937b3cb1bd1a3d028cc1b880c5c50f39
lijie <javalj@163.com>
2016-06-14 17:02:19 +0800

    改了几个名字

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # LoadmoreRefreshView
 
 #自己用的加载更多,做了个简单封装，有些bug不做解决
+暂时只做了ListView的
+
+GridView正在开发中
---
+++ ljLib/src/main/java/com/javalj/RefreshLayout/LGridView.java
@@ -0,0 +1,18 @@
+package com.javalj.RefreshLayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by lijie on 2016/6/14.
+ */
+public class LGridView extends GridView {
+    public LGridView(Context context) {
+        super(context);
+    }
+
+    public LGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
